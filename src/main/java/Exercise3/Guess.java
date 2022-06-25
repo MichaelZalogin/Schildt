0c4ba02b0bd22ed1dd3086be8a55345cb432dcf2
MichaelZalogin <zaloginmiha@gmail.com>
2022-06-25 16:59:55 +0300

    Update

--- src/main/java/Exercise3/Guess.java
+++ src/main/java/Exercise3/Guess.java
@@ -5,21 +5,33 @@ import java.io.IOException;
 public class Guess {
     public static void main(String[] args)
             throws IOException {
+
         char ch;
         char answer = 'K';
-        System.out.println("Задумана буква в диапазоне от A до Z.");
-        System.out.print("Попытайтесь угадать: ");
+        char ignore;
 
-        ch = (char) System.in.read();// Ввод символа
-        if (ch == answer)
-            System.out.println("Правильно!");
-        else {
-            System.out.print("...Извините, нужная буква находится ");
-            if (ch < answer) {
-                System.out.println("ближе к концу алфавита");
-            } else {
-                System.out.println("ближе к началу алфавита");
+        do {
+            System.out.println("Задумана буква в диапазоне от A до Z.");
+            System.out.print("Попытайтесь угадать: ");
+
+            ch = (char) System.in.read();// Получение символа от пользователя
+
+            //отбрасывание всех остальных символов во входном буфере
+            do {
+                ignore = (char) System.in.read();
+            } while (ignore != '\n');
+
+            if (ch == answer)
+                System.out.println("Правильно!");
+            else {
+                System.out.print("...Извините, нужная буква находится ");
+                if (ch < answer) {
+                    System.out.println("ближе к концу алфавита");
+                } else
+                    System.out.println("ближе к началу алфавита");
+                System.out.println("Попробуйте еще раз!\n");
             }
         }
+        while (answer != ch);
     }
 }
