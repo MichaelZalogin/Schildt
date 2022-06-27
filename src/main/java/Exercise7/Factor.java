deb8918fed33f40da3b21771ae53bc0182644a3f
MichaelZalogin <zaloginmiha@gmail.com>
2022-06-27 22:45:12 +0300

    Exercise 7

---
+++ src/main/java/Exercise7/Factor.java
@@ -0,0 +1,23 @@
+package Exercise7;
+
+public class Factor {
+    boolean isFactor(int a, int b) {
+        if ((b % a) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class IsFact {
+    public static void main(String[] args) {
+        Factor x = new Factor();
+        if (x.isFactor(2, 20)) {
+            System.out.println("2 - делитель");
+        }
+        if (x.isFactor(3, 20)) {
+            System.out.println("3 - делитель");
+        }
+    }
+}
